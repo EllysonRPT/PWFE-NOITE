520e7aa68a2bd40146f9b7f9e0c9bc42e0031bfe
EllysonRPT <reptile1284@gmail.com>
2023-10-10 22:03:40 -0300

    10/10

---
+++ ProVue/my-project-vue/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Meu Catálogo de Filmes</h1>
+    </header>
+    <main>
+
+      <CatalogoDeFilmes />
+      <CatalogoDeLivros />
+    </main>
+  </div>
+  <footer>
+    <h1>Minha Página com Vue.js</h1>
+  </footer>
+</template>
+  
+<script>
+import CatalogoDeFilmes from "@/components/CatalogoDeFilmes.vue";
+import CatalogoDeLivros from "./components/CatalogoDeLivros.vue";
+
+export default {
+  components: {
+    CatalogoDeFilmes,
+    CatalogoDeLivros
+  },
+};
+</script>
+  
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+
+header {
+
+  background-color: #35495e;
+  color: #eeafaf;
+  padding: 10px 0;
+}
+
+footer {
+  background-color: #35495e;
+  color: #bcebbf;
+  padding: 10px 0;
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+}
+</style>
--- past/my-first-server/src/app/app.module.ts
+++ past/my-first-server/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,10 +9,12 @@ import { HomeComponent } from './home/home.component';
 import { ContatoComponent } from './contato/contato.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { Route, Routes } from '@angular/router';
+import { FormularioComponent } from './formulario/formulario.component';
 const routes: Routes = [
   {path: '',component:HomeComponent},
   {path: 'Footer',component:FooterComponent},
   {path: 'Header',component:HeaderComponent},
+
   
 ];
 
@@ -23,11 +25,13 @@ const routes: Routes = [
     FooterComponent,
     HomeComponent,
     ContatoComponent,
-    SobreComponent
+    SobreComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
